b6b93a2cebe97ababc3846b40399c3f32d23e47b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-07-09 12:46:22 +0300

    add Task00, Task01

---
+++ Task00/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using static System.Console;
+
+Clear();
+
+Words ("аисв", new char[5]);
+
+void Words (string alfavit, char[] word, int length = 0) {
+	if (length == word.Length) {
+		WriteLine($"{new String(word)}");
+		return;
+	}
+	for (int i = 0; i < alfavit.Length; i ++) {
+		word[length] = alfavit[i];
+		Words(alfavit, word, length + 1);
+	}
+}
---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+using System;
+using static System.Console;
+Clear();
+WriteLine(String.Join(", ", Encrypt(new int[]{0, 1, 1, 1, 1, 0, 0, 0, 1 }, new int[]{2, 3, 3, 1 })));
+
+
+double[] Encrypt(int[] data, int[] info) {
+	double[] result = new double[info.Length];
+	int count = 0;
+	for (int i = 0; i < info.Length; i ++ )
+		for (int j = 0; j < info[i]; j ++) {
+			result[i] += data[count] * Math.Pow(2, info[i] - j - 1);
+			count ++;
+		}
+	return result;
+}
